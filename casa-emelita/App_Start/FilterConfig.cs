21435544041d7f1d34bacf11bbffc2b9c9e8898e
Jomiel <enriquez.jliquigan@gmail.com>
2023-09-14 10:42:17 +0800

    Add project files.

---
+++ casa-emelita/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace casa_emelita
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
