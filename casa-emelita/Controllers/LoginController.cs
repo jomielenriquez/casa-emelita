05293d32691f47ec5c99db47f141e439f4a1a5a5
Jomiel <enriquez.jliquigan@gmail.com>
2023-09-16 11:32:18 +0800

    Adding database and new controller

---
+++ casa-emelita/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace casa_emelita.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
